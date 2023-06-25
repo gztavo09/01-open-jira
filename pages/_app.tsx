506154ee285b16a6ddd38c987c94a36e7dab0377
Gustavo Arias <gjarias@arqsoft.com.pe>
2023-06-25 10:35:17 -0500

    diseño de pantalla principal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UIProvider>
-      <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
+import { NextPage } from "next"
 import { Layout } from "@/components/layouts"
-import { Typography } from "@mui/material"
+import { Card, CardHeader, Grid, Typography } from "@mui/material"
 
-export default function Home() {
+
+const Home: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>Holi</Typography>
+    <Layout title='Home - open Jira'>
+      <Grid container spacing={ 2 }>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Pendientes" />
+
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="En Progreso" />
+
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Completadas" />
+
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
+
+export default Home
